bee8b23756838a6f82f75e2ee2d5ddb4ec5242f9
PoloBustillo <leopoldobeguiluz1@hotmail.com>
2022-08-04 21:02:00 -0500

    ultimos cambios 2

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,26 +4,6 @@ import BoxGame from "./BoxGame";
 
 function Board() {
   return (
-<<<<<<< Updated upstream
-    <Container className="container-vertical text-center">
-      <Row className="mx-auto" style={{ width: "90%" }}>
-        <label>Jugador 1:</label>
-        <input></input>
-        <label>Jugador 2:</label>
-        <input></input>
-      </Row>
-      <Row className="vertical-center">
-        <div>
-          <h3>Turno: Jugador 1</h3>
-        </div>
-        {
-          //CODIGO JS
-          //ARREGLO DE (9 elementos)
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map((indice) => {
-            return <BoxGame></BoxGame>;
-          })
-        }
-=======
     <Container className="text-center board-container">
       <Row className="center">
         <BoxGame></BoxGame>
@@ -35,7 +15,6 @@ function Board() {
         <BoxGame></BoxGame>
         <BoxGame></BoxGame>
         <BoxGame></BoxGame>
->>>>>>> Stashed changes
       </Row>
     </Container>
   );
--- src/components/BoxGame.js
+++ src/components/BoxGame.js
@@ -4,17 +4,10 @@ import { Col } from "react-bootstrap";
 function BoxGame() {
   return (
     <Col
-<<<<<<< Updated upstream
-      className="border col-4"
-      style={{ height: "50px", position: "relative" }}
-    >
-      <span className="vertical-center">1</span>
-=======
       className="border col-4 border-pink box-container"
       style={{ height: "80px" }} //TRATAR DE EVITAR
     >
       <span className="purple center">X</span>
->>>>>>> Stashed changes
     </Col>
   );
 }

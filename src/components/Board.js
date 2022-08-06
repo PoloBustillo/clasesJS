24f6ee1e55d3bcf3dfc70d93bde9d1fc8a9cc390
PoloBustillo <leopoldobeguiluz1@hotmail.com>
2022-08-05 21:30:11 -0500

    react component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Container } from "react-bootstrap";
 import { Circle, XLg } from "react-bootstrap-icons";
 import BoxGame from "./BoxGame";
 
 function Board() {
-  const stateArray = ["X", "O", "O", "X", "X", "O", null, "X", "O"];
-  let player = "X";
+  const [gameState, setGameState] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+
+  let currentPlayer = "O";
   //Crear nuevo arreglo usando el anterior
-  let newStateArray = stateArray.map((value, position) => {
-    return <BoxGame selection={value} index={position}></BoxGame>;
+  let newStateArray = gameState.map((value, position) => {
+    return (
+      <BoxGame key={position} selection={value} index={position}></BoxGame>
+    );
   });
 
   //Dibujar nuevo arreglo en consola
@@ -20,10 +33,12 @@ function Board() {
         {newStateArray}
         <span className="mt-5">
           Turno:{" "}
-          {player === "O" ? (
+          {currentPlayer === "O" ? (
             <Circle color="purple"></Circle>
-          ) : (
+          ) : currentPlayer === "X" ? (
             <XLg color="purple"></XLg>
+          ) : (
+            <span></span>
           )}
         </span>
       </Row>
--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -1,29 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Container, Form, Row } from "react-bootstrap";
 import { Circle, XLg } from "react-bootstrap-icons";
 
 export const GameForm = () => {
+  const [firstPlayer, setFirstPlayer] = useState("");
+  const [secondPlayer, setSecondPlayer] = useState("");
+
   return (
     <Container className="text-center form-container">
       <Row>
         <Form className="mt-5">
           <Form.Group className="mb-3 w-50 m-auto" controlId="formBasicEmail">
             <Form.Label>
-              Jugador <XLg></XLg>
+              Jugador {firstPlayer} <XLg></XLg>
             </Form.Label>
-            <Form.Control type="text" placeholder="Nombre 1" />
+            <Form.Control
+              type="text"
+              placeholder="Nombre 1"
+              onChange={(event) => {
+                setFirstPlayer(event.target.value);
+              }}
+              value={firstPlayer}
+            />
           </Form.Group>
           <Form.Group className="mb-3 w-50 m-auto" controlId="formBasicEmail">
             <Form.Label>
-              Jugador <Circle></Circle>
+              Jugador {secondPlayer} <Circle></Circle>
             </Form.Label>
-            <Form.Control type="text" placeholder="Nombre 2" />
+            <Form.Control
+              onChange={(event) => {
+                setSecondPlayer(event.target.value);
+              }}
+              type="text"
+              placeholder="Nombre 2"
+              value={secondPlayer}
+            />
           </Form.Group>
-          <Button className="mx-2" variant="success" type="submit">
+          <Button
+            className="mx-2"
+            variant="success"
+            disabled={firstPlayer === "" || secondPlayer === ""}
+          >
             Nuevo Juego
-          </Button>
-          <Button className="mx-2" variant="info" type="submit">
-            Reiniciar
           </Button>
         </Form>
       </Row>

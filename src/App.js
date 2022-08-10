c82dfbd852fe55948a5e80c40998e380963cd749
PoloBustillo <leopoldobeguiluz1@hotmail.com>
2022-08-09 21:43:38 -0500

    llamar api gatitos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import NavigationBar from "./components/NavigationBar";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() { 
+function App() {
+  const URL =
+    "https://api.unsplash.com/search/photos?client_id=GS9NJjBSqA2w3ak6AOHAIYWDNDCcjQQwPtLNtUNswqY&query=kitten&color=black_and_white";
+
+  const [response, setResponse] = useState([]);
+
+  useEffect(() => {
+    //llamadas a servicios
+    //info de mi perfil
+    //info de mis blogs
+    //imagenes...
+    let getPhotosData = async () => {
+      let photosResponse = await axios({
+        method: "get",
+        url: URL,
+      });
+
+      let urlArray = photosResponse.data.results.map((photoData) => {
+        return photoData.urls.thumb;
+      });
+      setResponse(urlArray);
+      console.log(urlArray);
+    };
+    getPhotosData();
+  }, []);
+
   return (
     <>
-      <NavigationBar></NavigationBar>
+      MEMORAMA
+      <img src={response[0]}></img>
+      <img src={response[1]}></img>
+      <img src={response[2]}></img>
+      <img src={response[3]}></img>
+      <img src={response[4]}></img>
+      <img src={response[5]}></img>
+      <img src={response[6]}></img>
+      <img src={response[7]}></img>
+      <img src={response[8]}></img>
+      <img src={response[9]}></img>
     </>
   );
 }
--- src/components/Link.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Link(props) {
-  return <a style={{ color: props.colores }}>Link</a>;
-}
-
-export default Link;

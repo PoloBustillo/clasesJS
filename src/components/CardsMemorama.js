cc8feb3ff4442dccda1873392069dd478ecb7859
MariielDesign <mariel.graphicdesigner@gmail.com>
2022-08-10 22:35:17 -0500

    marielcanbios

--- src/components/CardsMemorama.js
+++ src/components/CardsMemorama.js
@@ -1,24 +1,27 @@
-import { Col, Container } from "react-bootstrap";
+import { Col, Container, Image } from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 
+
 function createImages(arrayImages) {
-  return(
-    arrayImages.map((url) => { //url1,url2,url3...
-      return (<Col><img src={url}></img></Col>);
-    })
-  );
+    return (
+        arrayImages.map((url) => { //url1,url2,url3...
+            return (<Col className="col-4 col-sm-3 border image-container"
+
+            ><Image src={url} className="image" ></Image></Col>);
+        })
+    );
 }
 
 function CardsMemorama({ imagen }) {
-  return (
-    <Container className="text-center board-container">
-      <Row className="center">
-       {
-        createImages(imagen)
-       }
-      </Row>
-    </Container>
-  );
+    return (
+        <Container className="text-center board-container">
+            <Row className="center">
+                {
+                    createImages(imagen)
+                }
+            </Row>
+        </Container>
+    );
 }
 
 export default CardsMemorama;

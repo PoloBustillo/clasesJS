b12eeb0fdd530675bf7f9f8827eb5cd009fde671
MariielDesign <mariel.graphicdesigner@gmail.com>
2022-08-11 23:10:58 -0500

    cambiosMariel_

--- src/components/CardsMemorama.js
+++ src/components/CardsMemorama.js
@@ -6,9 +6,16 @@ function random() {
 
 function createCol(url) {
   return (
-    <Col className="col-4 col-sm-3 border image-container">
-      <Image src={url} className="image"></Image>
-    </Col>
+    <Col className="col-4 col-sm-3 image-container flip-card">
+      <div class="flip-card-inner">
+        <div className="flip-card-front">
+          <h1>?</h1>
+           </div>
+        <div className="flip-card-back">
+          <Image src={url} className="image"></Image>
+        </div>
+      </div>
+    </Col >
   );
 }
 

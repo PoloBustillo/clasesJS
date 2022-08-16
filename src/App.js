803c6e2feeb640073590f39d9088551e12f81c33
juca004 <juca210784@gmail.com>
2022-08-15 23:43:32 -0500

    mi mensaje de modificaciones Juan

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import BoardMemorama from "./components/BoardMemorama";
 import { Container } from "react-bootstrap";
 import "./styles/App.css";
 import CardsMemorama from "./components/CardsMemorama";
-import { random } from "./utilities/helpers";
+import { getPhotosData } from "./utilities/helpers";
+
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("gatitos");
-  const URL =
-    "https://api.unsplash.com/search/photos?client_id=GS9NJjBSqA2w3ak6AOHAIYWDNDCcjQQwPtLNtUNswqY&query=" +
-    searchTerm;
   const [response, setResponse] = useState([]); //arreglo url ya formateadas
 
   useEffect(() => {
@@ -18,20 +15,9 @@ function App() {
     //info de mi perfil
     //info de mis blogs
     //imagenes...
-    let getPhotosData = async () => {
-      let photosResponse = await axios({
-        method: "get",
-        url: URL,
-      });
-
-      let urlArray = photosResponse.data.results.map((photoData) => {
-        return photoData.urls.thumb;
-      });
-      let newArrayConcat = urlArray.concat(urlArray);
-      let newArraySort = newArrayConcat.sort(random);
-      setResponse(newArraySort);
-    };
-    getPhotosData();
+    
+    getPhotosData(setResponse, searchTerm);
+    console.log(response);
   }, [searchTerm]);
 
   return (
--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -1,3 +1,4 @@
+import { axios } from "axios";
 /**
  *
  * @returns regresa un numero aleatorio de -1 a 1
@@ -5,3 +6,20 @@
 export function random() {
   return 0.5 - Math.random();
 }
+
+export let getPhotosData = async (setResponse, searchTerm) => {
+  const URL =
+  "https://api.unsplash.com/search/photos?client_id=GS9NJjBSqA2w3ak6AOHAIYWDNDCcjQQwPtLNtUNswqY&query=" +
+  searchTerm;
+  let photosResponse = await axios({
+    method: "get",
+    url: URL,
+  });
+
+  let urlArray = photosResponse.data.results.map((photoData) => {
+    return photoData.urls.thumb;
+  });
+  let newArrayConcat = urlArray.concat(urlArray);
+  let newArraySort = newArrayConcat.sort(random);
+  setResponse(newArraySort);
+};

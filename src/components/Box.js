b418005d0b0e8058af328d8d8bb1bbaca536c9dc
PoloBustillo <leopoldobeguiluz1@hotmail.com>
2022-08-16 23:35:09 -0500

    active class

--- src/components/Box.js
+++ src/components/Box.js
@@ -2,23 +2,22 @@ import React, { useState } from "react";
 import { Col, Image } from "react-bootstrap";
 
 function Box({ imagen, click, indice, showArray }) {
-  
   const [active, setActive] = useState(false);
   return (
     <Col
       onClick={(evento) => {
         let newArray = [...showArray];
-        if (newArray.length <2) {newArray.push(indice);
-          setActive(true)
-        click(newArray);
-        console.log(indice);} 
-        //guardar el nuevo estado si showArray.leght < 2
+        if (newArray.length < 2) {
+          newArray.push(indice);
+          setActive(true);
+          click(newArray);
+          console.log(indice);
+        }
         //sino es mayor o igual que 2, entonces verifica
-        
       }}
       className="col-4 col-sm-3 image-container flip-card"
     >
-      <div class={active?"flip-card-inner active": "flip-card-inner"}> 
+      <div class={active ? "flip-card-inner active" : "flip-card-inner"}>
         <div className="flip-card-front">
           <h1>?</h1>
         </div>

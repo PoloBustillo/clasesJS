d9a7286e194306d6b1cc35ed9559fb656c4986a5
PoloBustillo <leopoldobeguiluz1@hotmail.com>
2022-08-19 22:19:52 -0500

    datos

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 //TIPOS DE DATOS PRIMITIVOS
 
+// let - limitada por el scope (bloque de codigo)
+// var - se usa para identificar variables globales
+// const - variable que solo se asigna su valor una solo vez
+
 //let y var: variables que pueden cambiar su valor
 let STRING_LET; //declaro variable - creo espacio en memoria
-var STRING_VAR; //declaro variable - creo espacio en memoria
-STRING_LET = "valor string"; //asigno valor a variable
+var STRING_VAR = "asignada en la declaracion"; //declaro variable - creo espacio en memoria
+
+STRING_LET = 123; //asigno valor a variable
+//sumar
+STRING_LET = "reasignar valor";
+//expresar el resultado con un string
+
 //export: hacer visible elemento para los demas archivos
 //const: variable constante (no cambia), es decir no puede ser reasignada
 export const NULO = null;
 export const INDEFINIDO = undefined;
 export const STRING = "string";
 export const NUMERO = 12345;
+export const BIG_INT = 123451123010301203120302112031200321;
 export const BOOLEANO = true;
 
 //TIPOS DE ESTRUCTURAS
+
 export const OBJETO_O_JSON = {
   identificador: "valor string",
   identificador2: 1234,
@@ -21,11 +32,33 @@ export const OBJETO_O_JSON = {
   identificador5: () => {
     return "hola";
   },
+  identificador6: STRING_LET,
 };
+
+let colorFav = "Rosa";
+
+let miContacto = {
+  nombre: "Mario",
+  edad: 33,
+  dir: { calle: "2 de Octubre", numero: 13, cp: 622132 },
+  apellidos: ["primero", "segundo"],
+  llamar: () => {
+    console.log("llamando");
+  },
+  colorFavorito: colorFav,
+};
+
 //acceder a valores del objeto
 let objeto = OBJETO_O_JSON.identificador3.id.id;
 
-export const ARREGLO = [0, { id: "o" }, "dato3", [12, 13], true, 5];
+export const ARREGLO = [0, miContacto, "dato3", [12, 13], true, 5];
+
+let indices = [1, 3];
+let urls = ["qweoiqwie", "kqweiqwie", "qoweoqwe", "qwoeoqwe"];
+
+urls[miContacto.dir.cp];
+urls[indices[1]];
+
 //acceder a valores del arreglo
 let arreglo = ARREGLO[3][0];
 console.log(arreglo);

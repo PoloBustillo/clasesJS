650b786a37ef06e8eac2fb7ac5ceaf1b37b727eb
Venus Merino <psic.danieladiazmer@gmail.com>
2022-08-22 21:07:01 -0500

    daniela8

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,6 +3,7 @@ import { Col, Image } from "react-bootstrap";
 
 function Box({ imagen, click, indice, showArray, verificadas }) {
   const [active, setActive] = useState(false);
+const [estiloGris, setEstiloGris] = useState(false);
 
   const activeOrDisable = () => {
     if (showArray.includes(indice) || verificadas.includes(indice)) {
@@ -10,26 +11,36 @@ function Box({ imagen, click, indice, showArray, verificadas }) {
     } else {
       setActive(false);
     }
-  };
+  }
   useEffect(activeOrDisable, [showArray]);
+  
+  const convertirGris = () => {
+    if (verificadas.includes(indice)) {
+      setEstiloGris(true);
+    } else {
+      setEstiloGris(false);
+    }
+  }
+  ;
+  useEffect(convertirGris, [verificadas]);
 
   return (
     <Col
       onClick={() => {
         let newArray = [...showArray];
-        if (newArray.length < 2) {
+        if (newArray.length < 20) {
           newArray.push(indice);
           click(newArray);
         }
       }}
       className="col-4 col-sm-3 image-container flip-card"
     >
-      <div class={active ? "flip-card-inner active" : "flip-card-inner"}>
+      <div className={active ? "flip-card-inner active" : "flip-card-inner"}>
         <div className="flip-card-front">
           <h1>?</h1>
         </div>
         <div className="flip-card-back">
-          <Image src={imagen} className="image"></Image>
+          <Image src={imagen} className= {estiloGris ? "image estiloGris" : "image"}/>
         </div>
       </div>
     </Col>

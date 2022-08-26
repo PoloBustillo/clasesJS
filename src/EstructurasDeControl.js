9b39f04db282a6a4470bc59c2f0521b65f8933af
PoloBustillo <leopoldobeguiluz1@hotmail.com>
2022-08-25 21:17:11 -0500

    ejercicio 2

--- src/EstructurasDeControl.js
+++ src/EstructurasDeControl.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function EstructurasDeControl() {
-  return <div></div>;
+  let arreglo = ["Polo", "Juanito", "Mariel", "Daniela", "Dalton"];
+  function saludarATodos() {
+    //crear una funcion que me regrese un saludo para cada persona en el arreglo
+    // si la persona soy yo que me regrese un mejor saludo
+    // ["Super Hola Polo", "Hola Juanito", "Hola Mariel".....]
+  }
+  return <div>{saludarATodos()}</div>;
 }
 
 export default EstructurasDeControl;
